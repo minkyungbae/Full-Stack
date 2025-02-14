0d6a2b7f61d400f479ab65793c4f3497a79e1ded
배민경 <bmk0703@naver.com>
2025-02-14 21:30:12 +0900

    Add User(AbstractUser)

--- diary-project/backend/accounts/models.py
+++ diary-project/backend/accounts/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    # 원하는 필드를 추가
+    nickname = models.CharField(max_length=30, blank=True)
+    bio = models.TextField(blank=True)
+
+    # 객체의 문자열 표현 정리 : User를 view에서 쓸 때 username이 return 됨
+    def __str__(self):
+        return self.username
