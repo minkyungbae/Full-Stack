a8c525491f8104eb40fcb3aefd32c5a91ec8f0b1
배민경 <bmk0703@naver.com>
2025-02-17 20:59:01 +0900

    fix : RegisterView -> SignupView

--- diary-project/backend/accounts/views.py
+++ diary-project/backend/accounts/views.py
@@ -1,9 +1,32 @@
-from rest_framework import generics
-from .serializers import UserRegistrationSerializer
+from rest_framework import generics, status
+from .serializers import UserSignUpSerializer, UserLogInSerializer
 from django.contrib.auth import get_user_model
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
 User = get_user_model()
 
-class UserRegisterView(generics.CreateAPIView):
+# 회원 가입
+class UserSignUpView(generics.CreateAPIView):
     queryset = User.objects.all()
-    serializer_class = UserRegistrationSerializer
+    serializer_class = UserSignUpSerializer
+
+
+# 로그인
+class LogInView(generics.GenericAPIView):
+    permission_class = [permissions.AllowAny] # 인증되지 않은 사람도 접근 가능
+    serializer_class = UserLogInSerializer
+    
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            'refresh' : str(refresh),
+            'access' : str(refresh.access_token)
+        }, status=status.HTTP_200_OK)
+        
+    
+    
