3e7be415665f6f4135159ae7313df01be8c7eb27
배민경 <bmk0703@naver.com>
2025-02-17 20:58:31 +0900

    fix : Signup url

--- diary-project/backend/accounts/urls.py
+++ diary-project/backend/accounts/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.UserRegisterView.as_view(), name="register"), # 회원가입만 구현
-    path('login/', LogInView.as_view(), name="login"), # 로그인
+    path('signup', views.UserSignUpView.as_view(), name="signup"), # 회원가입만 구현
+    path('login/', views.LogInView.as_view(), name="login"), # 로그인
 ]
