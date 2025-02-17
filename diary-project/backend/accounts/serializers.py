1a244117bd28bcf791574dac18b1ab9eb518e4c7
배민경 <bmk0703@naver.com>
2025-02-17 20:58:15 +0900

    add : authenticate

--- diary-project/backend/accounts/serializers.py
+++ diary-project/backend/accounts/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
+from django.core.exceptions import ValidationError
 
 # settings.py에 선언했던 AUTH_USER_MODEL을 데려옴
 User = get_user_model()
 
-class UserRegistrationSerializer(serializers.ModelSerializer):
+# 회원 가입
+class UserSignUpSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(write_only=True) # 비밀번호 확인 / 필수 요소
 
     class Meta:
@@ -28,3 +30,26 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
+    
+# 로그인    
+class UserLogInSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(required=True) # 필수 입력
+    password = serializers.CharField(required=True, write_only=True) # 필수 입력
+    
+    class Meta:
+        model = User
+        fields = ('username', 'nickname', 'password', 'password2', 'bio',)
+    
+    def validate_Userpassword(self, password):
+        if not str() in password:
+            raise ValidationError("비밀번호에 문자는 필수입니다.")
+        if not int() in password:
+            raise ValidationError("비밀번호에 숫자는 필수입니다.")
+        return password
+    
+    def validate_Username(self, data):
+        user = authenticate(username=data['username'], password=data['password'])
+        if user is None:
+            raise ValidationError("아이디 또는 비밀번호가 일치하지 않습니다.")
+        data['user']=user
+        return data 
