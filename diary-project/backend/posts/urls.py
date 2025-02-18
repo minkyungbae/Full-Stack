faf908b75a71de6eae2fdc8739a5020f40949eee
배민경 <bmk0703@naver.com>
2025-02-18 20:44:59 +0900

    add : create url

--- diary-project/backend/posts/urls.py
+++ diary-project/backend/posts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ArticleListView.as_view(), name="articlelist"), # 글 목록 가기
+    path('create/', views.ArticleCreateView.as_view(), name="create"), # 글 생성
 ]
