f6b5784e6ec0738ca6c15319d88767f488e816d3
배민경 <bmk0703@naver.com>
2025-02-18 21:05:24 +0900

    fix : Serializer -> ModelSerializer

--- diary-project/backend/posts/serializers.py
+++ diary-project/backend/posts/serializers.py
@@ -1,9 +1,8 @@
 from rest_framework import serializers
-from django.core.exceptions import ValidationError
 from .models import Article
 
 # 기본 ArticleSeralizer
-class ArticleSerializer(serializers.Serializer):
+class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         field = "__all__"
