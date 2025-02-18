bd47a900b2dc8745d01e24203e4374aed6dc71c7
배민경 <bmk0703@naver.com>
2025-02-18 20:00:32 +0900

    add : logout url

--- diary-project/backend/accounts/urls.py
+++ diary-project/backend/accounts/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('signup', views.UserSignUpView.as_view(), name="signup"), # 회원가입만 구현
+    path('signup/', views.UserSignUpView.as_view(), name="signup"), # 회원가입만 구현
     path('login/', views.LogInView.as_view(), name="login"), # 로그인
+    path('logout/', views.LogOutView.as_view(), name="logout") # 로그아웃
 ]
