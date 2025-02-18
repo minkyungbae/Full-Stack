207f8a7302754515dfa2bb24bb0cb64dc6fd5c9c
배민경 <bmk0703@naver.com>
2025-02-18 21:40:51 +0900

    add : def create

--- diary-project/backend/posts/serializers.py
+++ diary-project/backend/posts/serializers.py
@@ -8,3 +8,8 @@ class ArticleSerializer(serializers.ModelSerializer):
         field = "__all__"
         read_only_fields = ('author',)
         exclude = ("created_date", "updated_date")
+        
+    def create(self, validated_data):
+        author = self.context['request'].user
+        validated_data['author'] = author # validated_data 에 author 추가
+        return super().create(validated_data) # super().create() 호출
