b0a2f9c2169a7cd9c03d28675ca4817acaf44e80
배민경 <bmk0703@naver.com>
2025-02-18 20:58:46 +0900

    add : def create

--- diary-project/backend/posts/serializers.py
+++ diary-project/backend/posts/serializers.py
@@ -2,9 +2,14 @@ from rest_framework import serializers
 from django.core.exceptions import ValidationError
 from .models import Article
 
-# 글쓰기
+# 기본 ArticleSeralizer
 class ArticleSerializer(serializers.Serializer):
     class Meta:
         model = Article
         field = "__all__"
+
+    def create(self, validated_data):
+        author = self.context['request'].user
         
+        article = Article.objects.create(author=author, **validated_data)
+        return article
