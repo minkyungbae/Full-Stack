1f3488aace772513e6b5a79abb8116e180b2f814
배민경 <bmk0703@naver.com>
2025-02-18 21:32:12 +0900

    add : author

--- diary-project/backend/posts/models.py
+++ diary-project/backend/posts/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.conf import settings
 
 class Article(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
