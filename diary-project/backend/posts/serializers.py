91fbcda82a3e99fb8b00ba5a0576f3ed76ec02fb
배민경 <bmk0703@naver.com>
2025-02-20 20:00:00 +0900

    add : ArticleUpdateSerializer

--- diary-project/backend/posts/serializers.py
+++ diary-project/backend/posts/serializers.py
@@ -13,3 +13,28 @@ class ArticleSerializer(serializers.ModelSerializer):
         author = self.context['request'].user
         validated_data['author'] = author # validated_data 에 author 추가
         return super().create(validated_data) # super().create() 호출
+    
+# Update serializer
+class ArticleUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        field = ["title", "content"]
+        read_only_fields = ("author",)
+        exclude = ("created_date", "updated_date")
+        
+    def valiate(self, data):
+        instance = self.instance
+        request = self.context.get('request')
+        if request and request.user != instance.author:
+            raise serializers.ValidationError("글 작성자가 아니시군요?? 글 작성자만 수정 가능합니다.")
+        return data
+    
+    def update(self, instance, validated_data):
+        """title update, 기본값은 본래의 title"""
+        instance.title = validated_data.get('title', instance.title)
+        """content update, 기본값은 본래의 content"""
+        instance.content = validated_data.get('content', instance.content)
+        instance.save()
+        return instance
+        
+            
