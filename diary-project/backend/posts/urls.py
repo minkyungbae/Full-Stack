132099f7b547b7a5b93fd8bb6f52e4d31ed8f6d3
배민경 <bmk0703@naver.com>
2025-02-20 20:14:35 +0900

    add : delete url

--- diary-project/backend/posts/urls.py
+++ diary-project/backend/posts/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.ArticleListView.as_view(), name="articlelist"), # 글 목록 가기
     path('create/', views.ArticleCreateView.as_view(), name="create"), # 글 생성
     path('update/<int:pk>/', views.ArticleUpdateView.as_view(), name="update"), # 글 수정
+    path('delete/<int:pk>/', views.ArticleDeleteView.as_view(), name="delete"), # 글 삭제
     
 ]
