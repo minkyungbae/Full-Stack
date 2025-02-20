d24a04013a07d2a9381bec61ebe157419b928913
배민경 <bmk0703@naver.com>
2025-02-20 19:47:24 +0900

    add : update url

--- diary-project/backend/posts/urls.py
+++ diary-project/backend/posts/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.ArticleListView.as_view(), name="articlelist"), # 글 목록 가기
     path('create/', views.ArticleCreateView.as_view(), name="create"), # 글 생성
+    path('update/', views.ArticleUpdateView.as_view(), name="update"), # 글 수정
+    
 ]
